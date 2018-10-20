aaa6b1df187847f274baf5aebf5569a9c25a01a4
Ji S <j2yshin@gmail.com>
2018-10-20 18:08:48 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,13 +5,12 @@ def reformat_languages(languages)
   new_hash = {}
   new_hash = languages[:oo].merge(languages[:functional])
   
-  new_hash.each do |language_name, type|
-    new_hash[language_name][:style] = []
+  new_hash.each do |name, type|
+    new_hash[name][:style] = []
     languages.each do |style, data|
-      data.each do |language_name, language_type|
-        binding.pry
+      data.each do |language_name, language_type_data|
         if languages[style].has_key?(language_name)
-          new_hash[language_name][:style] << style
+          new_hash[name][:style] << style
         end
       end
     end
