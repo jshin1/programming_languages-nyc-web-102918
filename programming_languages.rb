f03c7472c2c7e0143b8a4d155db831d41c84c7c9
Ji S <j2yshin@gmail.com>
2018-10-20 18:01:55 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,27 +1,6 @@
 require 'pry'
 
 def reformat_languages(languages)
-  # your code here
-  
-  new_hash = {}
-  new_hash = languages[:oo].merge(languages[:functional])
-  
-  languages.each do |style, language_data|
-    language_data.each do |language_name, type|
-      new_hash[language_name][:style] = []
-      
-      # insert something here because the array is resetting to [] every time
-      
-      
-      if languages[style].has_key?(language_name)
-        new_hash[language_name][:style] << style
-      end
-    end
-  end
-  new_hash
-end
-
-def reformat_languages(languages)
   
   new_hash = {}
   new_hash = languages[:oo].merge(languages[:functional])
@@ -37,13 +16,4 @@ def reformat_languages(languages)
     end
   end
   new_hash
-end
-
-
-
-
-
-
-
-
 end
