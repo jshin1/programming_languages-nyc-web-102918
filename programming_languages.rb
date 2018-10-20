ddbbcd12b1d89132cab1f527e9dac7f34a707ecb
Ji S <j2yshin@gmail.com>
2018-10-20 18:01:38 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -26,8 +26,18 @@ def reformat_languages(languages)
   new_hash = {}
   new_hash = languages[:oo].merge(languages[:functional])
   
-  new_hash.each do |language_name, language_data|
-    
+  new_hash.each do |language_name, type|
+    language_name[:style] = []
+    languages.each do |style, data|
+      data.each do |language_name, language_type|
+        if languages[style].has_key?(language_name)
+          new_hash[language_name][:style] << style
+        end
+      end
+    end
+  end
+  new_hash
+end
 
 
 
